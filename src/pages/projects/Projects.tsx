85aa71baf8994411d1c9963861c16be7e4768923
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-22 16:56:05 -0300

    feat: :sparkles: add projects page

---
+++ src/pages/projects/Projects.tsx
@@ -0,0 +1,27 @@
+import { Box, Heading, SimpleGrid } from '@chakra-ui/react';
+
+import Header from '../../components/header/Header';
+import ProjectCard from '../../components/project-card/ProjectCard';
+
+export default function Projects() {
+  return (
+    <Box minH="100vh" bgImage="url(src/assets/dark-sky.gif)" bgSize="cover">
+      <Header />
+
+      <Heading
+        margin="1.5em"
+        width="fit-content"
+        color="white"
+        borderBottom="1px solid white"
+        as="h1"
+      >
+        Projetos
+      </Heading>
+
+      <SimpleGrid margin="1.5m" columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="2em">
+        <ProjectCard />
+        <ProjectCard />
+      </SimpleGrid>
+    </Box>
+  );
+}
