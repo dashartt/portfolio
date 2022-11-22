07e3e88ef3b9d01b41eb656e0701ee1fe764cb59
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-22 16:55:50 -0300

    feat: :sparkles: add header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+import signature from '../../assets/signature-w.png';
+
+export default function Header() {
+  return (
+    <HStack
+      width={{ base: '80vw', lg: '30vw' }}
+      margin="auto"
+      justifyContent="space-between"
+    >
+      <Image width="5em" src={signature} alt="logo do portfolio" />
+
+      <Text borderBottom="1px solid white" color="white">
+        <Link to="/">Homepage</Link>
+      </Text>
+      <Text borderBottom="1px solid white" color="white">
+        <Link to="/projects">Projects</Link>
+      </Text>
+      <Text borderBottom="1px solid white" color="white">
+        <Link to="/about-me">About</Link>
+      </Text>
+    </HStack>
+  );
+}
