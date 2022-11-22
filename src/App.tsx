c15e122a1e52f1c0e5fe91f2642bd620b08ee75d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-22 16:57:35 -0300

    feat: :sparkles: config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,17 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import logo from './logo.svg';
+import Home from './pages/home/Home';
+import Projects from './pages/projects/Projects';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
