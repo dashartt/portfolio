4794da20809f7c842516e51ceb44cb9f1e9ed9f0
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-17 04:29:06 -0300

    fix: :adhesive_bandage: change router

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,18 @@ import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
